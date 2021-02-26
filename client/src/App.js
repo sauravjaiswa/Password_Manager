415d350744c34462d673a192a335d9e15c9aac97
sauravjaiswa <sauravjaiswal520@gmail.com>
2021-02-27 02:49:04 +0530

    Connected MySQL database and implemented further

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import Axios from "axios";
 
 function App() {
+
+  const [password,setPassword]=useState('')
+  const [title,setTitle]=useState('')
+
+  const addPassword=()=>{
+    Axios.post("http://localhost:3001/addpassword",{password: password,title: title})
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="AddingPassword">
+        <input type="text" 
+        placeholder="Ex. password123"
+        onChange={(event)=>{setPassword(event.target.value);
+        }}/>
+        <input type="text" 
+        placeholder="Ex. Signal"
+        onChange={(event)=>{setTitle(event.target.value);
+        }}/>
+        <button onClick={addPassword}>Add Password</button>
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
 const express=require("express");
 const app=express();
+const mysql=require('mysql');
+const cors=require("cors");
 const PORT=3001;
 
-app.get("/",(req,res)=>{
-    res.send("Hello World!! My first project on JS");
+app.use(cors());
+app.use(express.json())
+
+const db=mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'SYSTEM123',
+    database: 'PasswordManager',
+});
+
+app.post("/addpassword",(req,res)=>{
+    const {password,title}=req.body;
+    db.query("INSERT INTO passwords (password,title) VALUES (?,?)",
+    [password,title],
+    (err,result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.send("Success");
+        }
+    });
 });
 
 app.listen(PORT,()=>{
